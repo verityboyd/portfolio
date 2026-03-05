78b61365049cac7f91a38cb9ade6367ba53c2fa1
Verity <verity.boyd@edu.sait.ca>
2026-03-04 21:39:42 -0700

    changed bg-gradient to bg-linear per tailwind suggestion

--- app/page.js
+++ app/page.js
@@ -14,14 +14,14 @@ export default function Page() {
       <header>
         <Navbar />
         <Header />
-        <Separator className="max-w-7xl mx-auto bg-gradient-to-r from-[#A07CFE] to-[#FE8FB5]" />
+        <Separator className="max-w-7xl mx-auto bg-linear-to-r from-[#A07CFE] to-[#FE8FB5]" />
       </header>
       <About />
-      <Separator className="max-w-7xl mx-auto bg-gradient-to-r from-[#A07CFE] to-[#FE8FB5]" />
+      <Separator className="max-w-7xl mx-auto bg-linear-to-r from-[#A07CFE] to-[#FE8FB5]" />
       <Projects />
-      <Separator className="max-w-7xl mx-auto bg-gradient-to-r from-[#A07CFE] to-[#FE8FB5]" />
+      <Separator className="max-w-7xl mx-auto bg-linear-to-r from-[#A07CFE] to-[#FE8FB5]" />
       <Resume />
-      <Separator className="max-w-7xl mx-auto bg-gradient-to-r from-[#A07CFE] to-[#FE8FB5]" />
+      <Separator className="max-w-7xl mx-auto bg-linear-to-r from-[#A07CFE] to-[#FE8FB5]" />
       <Footer />
     </main>
   );
